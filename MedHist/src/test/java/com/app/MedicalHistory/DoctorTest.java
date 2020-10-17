a7be58f9ccdd7b2f1934f585b8fa0a3f2e0f6adf
katherineggs <katherinegarcia@ufm.edu>
2020-10-16 22:31:00 -0600

    adds manage access template

--- MedHist/src/test/java/com/app/MedicalHistory/DoctorTest.java
+++ MedHist/src/test/java/com/app/MedicalHistory/DoctorTest.java
@@ -20,11 +20,39 @@ class DoctorTest {
 //
     @Test
     @Parameters({"lastName"})
-        public void userTypeTest(String lastName) {
+        public void lastNameTest(String lastName) {
         doc.setLastName(lastName);
         String LastNAme = doc.getLastName();
         Assert.assertEquals(lastName, LastNAme); //??????????
     }
 
+    @Test
+    @Parameters({"Age"})
+    public void ageTest(int age) {
+        doc.setAge(age);
+        int getAge = doc.getAge();
+        Assert.assertEquals(age, getAge);
+    }
+    @Test
+    @Parameters({"specialty"})
+    public void specialityTest(String specialty) {
+        doc.setSpecialty(specialty);
+        String getSpec = doc.getSpecialty();
+        Assert.assertEquals(specialty, getSpec);
+    }
+    @Test
+    @Parameters({"ClinicAddress"})
+    public void addressTest(String address) {
+        doc.setClinicAddress(address);
+        String getClinicAddress = doc.getClinicAddress();
+        Assert.assertEquals(address, getClinicAddress);
+    }
+    @Test
+    @Parameters({"TimeWPatient"})
+    public void timeWPTest(int time) {
+        doc.setTimeWPatient(time);
+        int getTime = doc.getTimeWPatient();
+        Assert.assertEquals(time, getTime);
+    }
 
 }
