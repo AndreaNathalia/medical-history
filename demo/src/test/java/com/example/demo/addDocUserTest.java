e9c18de89e0dbf90496557566ec5e53c4a0e317e
katherineggs <katherinegarcia@ufm.edu>
2020-11-21 23:23:08 -0600

    comments

--- demo/src/test/java/com/example/demo/addDocUserTest.java
+++ demo/src/test/java/com/example/demo/addDocUserTest.java
@@ -1,12 +1,10 @@
 package com.example.demo;
 
-import org.testng.annotations.DataProvider;
 
 public class addDocUserTest {
-    @DataProvider(name = "addDoctorUser")
-    public Object[][] dataDocs() {
-        return new Object [][] {
-                {"uno", "dos"}
-        };
-    }
+    // MISSING
+//    ADDER FUNCTION
+//    PATIENT ADDER
+//    ADD PATIENTS
+//    DELETE PATIENTS
 }
