bdfabe1c552484eb873f69caeba8d5c7bf8eba4c
katherineggs <katherinegarcia@ufm.edu>
2020-11-24 22:15:55 -0600

    adds sTest file

---
+++ demo/src/test/java/com/example/demo/sTest.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sTest {
+    WebDriver driver = new ChromeDriver();
+}
